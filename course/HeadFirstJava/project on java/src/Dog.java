0ed6bb59224c8e13f90fdda7ed0b76a0b159639d
Zhomart <zhomart_aripbai@mail.ru>
2020-10-21 12:03:10 +0600

    just testing commit with SSH

--- course/HeadFirstJava/project on java/src/Dog.java
+++ course/HeadFirstJava/project on java/src/Dog.java
@@ -10,7 +10,7 @@
     }
     public static void main (String[] args) {
         Dog d = new Dog();
-        d.bark(3); // данная тройка 3 является аргументом
+        d.bark(4); // данная тройка 3 является аргументом
 
     }
 
